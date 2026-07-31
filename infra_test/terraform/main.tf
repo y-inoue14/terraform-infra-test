21c8e28c2741d29b7d3ff66072559eae49fa7724
y-inoue14 <y-inoue@isbb-jp.com>
2026-07-31 14:46:55 +0900

    Add files via upload

---
+++ infra_test/terraform/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_version = ">= 1.8.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-3"
+
+  default_tags {
+    tags = {
+      Project   = "InfrastructureTest"
+      ManagedBy = "Terraform"
+      Owner     = "YuraInoue"
+    }
+  }
+}
+
+locals {
+  common_js = templatefile(
+    "${path.module}/templates/common.js.tftpl",
+    {
+      lambda_function_url = aws_lambda_function_url.issue_url.function_url
+    }
+  )
+}
+
+resource "local_file" "common_js" {
+  filename = "${path.module}/static/common.js"
+
+  content = templatefile(
+    "${path.module}/templates/common.js.tftpl",
+    {
+      lambda_function_url = aws_lambda_function_url.issue_url.function_url
+    }
+  )
+}
---
+++ infra_test/terraform/s3.tf
@@ -0,0 +1,131 @@
+########################################
+# 静的サイト用S3
+########################################
+
+resource "aws_s3_bucket" "website" {
+  bucket = "infra-test-2026-isbb-osaka"
+}
+
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  depends_on = [
+    aws_s3_bucket_public_access_block.website
+  ]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = "*"
+
+        Action = [
+          "s3:GetObject"
+        ]
+
+        Resource = [
+          "${aws_s3_bucket.website.arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
+########################################
+# staticフォルダを自動アップロード
+########################################
+
+locals {
+  static_files = fileset("${path.module}/static", "**")
+}
+
+resource "aws_s3_object" "static" {
+
+  depends_on = [
+    local_file.common_js
+  ]
+  for_each = local.static_files
+
+  bucket = aws_s3_bucket.website.id
+
+  key = each.value
+
+  source = (
+    each.value == "common.js"
+    ? local_file.common_js.filename
+    : "${path.module}/static/${each.value}"
+  )
+
+  etag = filemd5("${path.module}/static/${each.value}")
+
+  content_type = lookup(
+    {
+      html = "text/html"
+      js   = "application/javascript"
+      css  = "text/css"
+      json = "application/json"
+      png  = "image/png"
+      jpg  = "image/jpeg"
+      jpeg = "image/jpeg"
+      svg  = "image/svg+xml"
+    },
+    reverse(split(".", each.value))[0],
+    "binary/octet-stream"
+  )
+}
+
+########################################
+# CSV保存用S3
+########################################
+
+resource "aws_s3_bucket" "storage" {
+  bucket = "infra-test-2026-isbb-osaka-storage"
+}
+
+resource "aws_s3_bucket_cors_configuration" "storage" {
+
+  bucket = aws_s3_bucket.storage.id
+
+  cors_rule {
+
+    allowed_methods = [
+      "PUT",
+      "GET",
+      "HEAD"
+    ]
+
+    allowed_headers = [
+      "*"
+    ]
+
+    allowed_origins = [
+  "http://${aws_s3_bucket_website_configuration.website.website_endpoint}"
+]
+
+    expose_headers = [
+      "ETag"
+    ]
+
+    max_age_seconds = 3000
+  }
+}
